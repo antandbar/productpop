59654adc51b8b04792bc76d944a9be180fbf3b7f
Antonio Andreu <antonio.andreu.barreiro@gmail.com>
2022-02-15 17:19:37 +0100

    feat: maquetación login

--- product-list/ProductView.js
+++ product-list/ProductView.js
@@ -1,14 +1,11 @@
-export function buildProductView(product) {
-  let theadTemplate = ""; 
-    
-
+export function buildProductView(product) {    
   let productTemplate =
     `
             <th scope="row">${product.id}</th>
             <td><img src="${product.image}"></img></td>
             <td>${product.name}</td>
             <td>${product.description}</td>
-            <td>${product.price}</td>
+            <td>${product.price}€</td>
             <td>${product.buySell}</td>
             <td><a href="/productDetail.html?id=${product.id}">Ver</a></td>      
   `;
@@ -17,14 +14,6 @@ export function buildProductView(product) {
 }
 
 export function buildproductDetailView(product) {
-
-  let productTemplate2 = `
-    <img src="${product.image}"></img>
-    <p>Nombre del producto: ${product.name}</p>
-    <p>Descripción del producto: ${product.description}</p>
-    <p>Precio del producto: ${product.price}</p>
-    <p>El producto está en compra o venta?: ${product.buySell}</p>
-  `;
 
   let productTemplate = `
     <div class="card" style="width: 18rem;">
